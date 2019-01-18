bd2c9bec1a3ea91b3aa2390f36ee9227669941c8
t-g-j <t.g.j@sol.dk>
2019-01-18 16:56:08 +0100

    Made new functions in matlab and a document for counting pokes in dataset

--- AI/AI_dataprocessing.m
+++ AI/AI_dataprocessing.m
@@ -1,14 +1,58 @@
 %% AI project (Artificial skin) data processing
-%plot(x30_5mm_0_0_50.b1)
+load('/home/tgj/Documents/9.Semester/AI/AI_variables.mat') 
 
-plot(x30_5mm_0_0_50.b2)
+
+%% 3mm
+%processData(x30_3mm_0_0_50.b1)
+%data=x30_3mm_0_0_50.b1;
+%processData(x30_3mm_0_0_50.b2)
+
+%%
+processData(x30_3mm_1_1_50.b1)
+processData(x30_3mm_1_1_50.b2)
+
+processData(x30_3mm_neg1_1_50.b1)
+processData(x30_3mm_neg1_1_50.b2)
+
+
+processData(x30_3mm_neg1_neg1_50.b1)
+processData(x30_3mm_neg1_neg1_50.b2)
+
+% 5mm
+processData(x30_5mm_0_0_50.b1)
+processData(x30_5mm_0_0_50.b2)
+
+processData(x30_5mm1_neg1_50.b1)
+processData(x30_5mm1_neg1_50.b2)
+
+processData(x30_5mm_1_1_50.b1)
+processData(x30_5mm_1_1_50.b2)
+
+processData(x30_5mm_0_0_50.b1)
+processData(x30_5mm_0_0_50.b2)
+
+% 10mm
+processData(x30_10mm_0_0_50.b1)
+processData(x30_10mm_0_0_50.b2)
+
+processData(x30_10mm_1_1_50.b1)
+processData(x30_10mm_1_1_50.b2)
+
+processData(x30_10mm0_0_50.b1)
+processData(x30_10mm0_0_50.b2)
+%%
+processData(x30_5mm_0_0_50.b1)
+
+function processed = processData(data)
+
+plot(data)
 title('original')
 xlabel('sample')
 ylabel('Value')
 %% Remove outliers
 
 figure
-new = removeOutliers(x30_5mm_0_0_50.b2);
+new = removeOutliers(data);
 plot(new)
 title('removed outliers')
 xlabel('sample')
@@ -17,7 +61,7 @@ ylabel('Value')
 %% Lowpass filter on removed outliers
 figure
 Fs = 10000; % PROPELY NOT CORRECT
-[Y,D] = lowpass(new,100,Fs);
+[Y,D] = lowpass(new,1,Fs);
 newOut = filter(D,new);
 plot(newOut)
 title('Applied lowpass')
@@ -42,12 +86,12 @@ title('filtfilt')
 %plot(newOut)
 %
 
-
+end
 
 
 %% Finding outliers
 function noOutLier = removeOutliers(x)
-STOPFLAG = true(1)
+STOPFLAG = true(1);
     while STOPFLAG
     tf = isoutlier(x);
     index = find(tf,1);
